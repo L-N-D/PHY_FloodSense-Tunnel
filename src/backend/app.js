cd075d72261a1aaf571a54cc622400800cd69fde
NDUY <lnhatduy27@gmail.com>
2025-12-08 11:14:51 +0700

    chore

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,7 +4,10 @@ import cookiesParser from 'cookie-parser';
 import commonRoute from './routes/index.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",   // domain FE
+    credentials: true,                 // QUAN TRỌNG
+}));
 app.use(json());
 app.use(cookiesParser());
 
