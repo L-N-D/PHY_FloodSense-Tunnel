494ff1f977f02db3eed687bbeff1fcb5f624b415
N.Duy_Computer <lnhatduy27@gmail.com>
2025-12-16 11:58:01 +0700

    help: fix mqtt

--- src/backend/modules/auth/auth.routes.js
+++ src/backend/modules/auth/auth.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { authVerifyToken } from './auth.middleware.js';
-import { loginController, logoutController, registerController,saveDeviceTokenController } from './auth.controller.js';
+import { loginController, logoutController, registerController } from './auth.controller.js';
 
 const router = express.Router();
 
 router.post('/login', loginController);
 router.post('/logout', authVerifyToken, logoutController);
 router.post('/register', registerController);
-router.post('/save-device-token', authVerifyToken, saveDeviceTokenController);
+// router.post('/save-device-token', authVerifyToken, saveDeviceTokenController);
 // router.get('/profile', authAccessToken);
 
 const authRouter = router;
